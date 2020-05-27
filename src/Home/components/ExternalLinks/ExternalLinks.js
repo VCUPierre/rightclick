df2bfa403a392d299507b0a0ecc3a778c4748978
Pierre Randall <randalltp@mymail.vcu.edu>
2020-05-26 20:28:19 -0400

    fixed banner position for all devices

--- src/Home/components/ExternalLinks/ExternalLinks.js
+++ src/Home/components/ExternalLinks/ExternalLinks.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import { Button, Label } from 'semantic-ui-react'
+import { useMediaPredicate } from "react-media-hook";
+import '../Links/Links.css'
 
 const ExternalLinks = (props) => {
+    const xsScreen = useMediaPredicate("(max-width: 375px)");
+    const mdScreen = useMediaPredicate("(max-width: 768px)");
+    const lgScreen = useMediaPredicate("(max-width: 1024px)");
+
     return <div>
         {props.link.featured 
         ?
         <div>
-            <Label as='a' color='green' ribbon className="bannerPosition">
+            <Label as='a' color='green' ribbon className={xsScreen ? "bannerPositionS" : mdScreen ? "bannerPositionM" : lgScreen ? "bannerPositionL" : "bannerPositionXL"}>
               {props.link.featuredText}
             </Label>
             <Button 
--- src/Home/components/InternalLinks/InternalLinks.js
+++ src/Home/components/InternalLinks/InternalLinks.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import { Button, Header, Image, Modal, Label } from 'semantic-ui-react'
+import { useMediaPredicate } from "react-media-hook";
+import '../Links/Links.css'
 
-const ModalModalExample = (props) => (
-  <div>
+const ModalModalExample = (props) => {
+  const xsScreen = useMediaPredicate("(max-width: 375px)");
+  const mdScreen = useMediaPredicate("(max-width: 768px)");
+  const lgScreen = useMediaPredicate("(max-width: 1024px)");
+
+  return <div>
     {props.link.featured 
     ?
     <div>
-      <Label as='a' color='green' ribbon className="bannerPosition">
+      <Label as='a' color='green' ribbon className={xsScreen ? "bannerPositionS" : mdScreen ? "bannerPositionM" : lgScreen ? "bannerPositionL" : "bannerPositionXL"}>
               {props.link.featuredText}
       </Label>
       
@@ -42,6 +48,6 @@ const ModalModalExample = (props) => (
     </Modal>
   }
 </div>
-)
+}
 
 export default ModalModalExample
