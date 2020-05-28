51a1b6b5c087c909c6c9405c5873ddb0974c8c19
vcuPierre <randalltp@mymail.vcu.edu>
2020-05-28 00:07:14 -0400

    adding data for social media links

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,22 @@
 export const TITLE = "Something"
+export const SOCIAL_LINKS = {
+    group: [{
+        name: "Instagram",
+        link: "instagram.com"
+    },
+    {
+        name: "Facebook",
+        link: "facebook.com"
+    },
+    {
+        name: "Twitter",
+        link: "twitter.com"
+    },
+    {
+        name: "Sound Cloud",
+        link: "soundcloud.com"
+    }
+]}
 export const LINK_GROUP_1 = {
     groupName: "Music Links",
     group: [{
@@ -63,19 +81,3 @@ export const LINK_GROUP_2 = {
         }
     ]
 }
-// export const SOCIAL_INSTAGRAM = {
-//     name: "Instagram",
-//     link: "instagram.com"
-// } 
-// export const SOCIAL_FACEBOOK = {
-//     name: "Facebook",
-//     link: "facebook.com"
-// }
-// export const SOCIAL_TWITTER = {
-//     name: "Twitter",
-//     link: "twitter.com"
-// }
-// export const SOCIAL_OTHER = {
-//     name: "Sound Cloud",
-//     link: "soundcloud.com"
-// }
