63834ed445eb5fc78a615797e2bab99cb8cdb7e5
vcuPierre <randalltp@mymail.vcu.edu>
2020-05-28 23:30:58 -0400

    removed stupid line

--- src/Home/components/ModalHeader/ModalHeader.js
+++ src/Home/components/ModalHeader/ModalHeader.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import { Header } from 'semantic-ui-react'
+import './ModalHeader.css'
 
 const ModalHeader = (props) => {
-    return <Header as='h2' textAlign='center'>{props.data.name}</Header>
+    return <Header as='h2' textAlign='center' className='RCborderNone'>{props.data.name}</Header>
 }
 
 export default ModalHeader
