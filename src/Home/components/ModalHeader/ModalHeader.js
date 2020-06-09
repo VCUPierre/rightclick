380f10ea12baf218678289b7855ccdae10a04119
vcuPierre <randalltp@mymail.vcu.edu>
2020-06-08 23:49:24 -0400

    fixed modal header

--- src/Home/components/ModalHeader/ModalHeader.js
+++ src/Home/components/ModalHeader/ModalHeader.js
@@ -3,7 +3,7 @@ import { Header } from 'semantic-ui-react'
 import './ModalHeader.css'
 
 const ModalHeader = (props) => {
-    return <Header as='h2' textAlign='center' className='RCborderNone'>{props.data.name}</Header>
+    return <Header as='h1' textAlign='center' className='RCborderNone RCtopPushoff'>{props.data.name}</Header>
 }
 
 export default ModalHeader
