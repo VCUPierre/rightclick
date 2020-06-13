870f329ab47ff01677685ffdaefd5672466de56d
vcuPierre <randalltp@mymail.vcu.edu>
2020-06-13 19:46:19 -0400

    Updates

---
+++ precache-manifest.9343776731f71a32e93b50c14b70b9f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "777699231764006094204799390834f5",
+    "url": "/rightclick/index.html"
+  },
+  {
+    "revision": "935e7fbb9f1d2fcee259",
+    "url": "/rightclick/static/css/main.96babb6f.chunk.css"
+  },
+  {
+    "revision": "35eac028e4a5b9ebd301",
+    "url": "/rightclick/static/js/2.31baf359.chunk.js"
+  },
+  {
+    "revision": "59eee5a20441efe8cf9652b2cc007836",
+    "url": "/rightclick/static/js/2.31baf359.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "935e7fbb9f1d2fcee259",
+    "url": "/rightclick/static/js/main.bb87056b.chunk.js"
+  },
+  {
+    "revision": "0b47fc163b02297681cc",
+    "url": "/rightclick/static/js/runtime-main.971231fc.js"
+  }
+]);
