31b88734e0a46c1480980eac320c1b6bbdfcd410
vcuPierre <randalltp@mymail.vcu.edu>
2020-06-24 00:23:49 -0400

    added logo

--- src/Home/components/Home.js
+++ src/Home/components/Home.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import {Grid, GridColumn} from 'semantic-ui-react'
+import {Grid, GridColumn, Segment, Image} from 'semantic-ui-react'
 import Profile from './Profile/Profile'
 import LinkGroup from './LinkGroup/LinkGroup'
 import { SOCIAL_LINKS, LINK_GROUP_1, LINK_GROUP_2 } from '../../constants.js'
+import Logo from '../../assets/Logo.png'
 
 const Home = (props) => (
     <Grid centered columns={2}>
@@ -17,6 +18,17 @@ const Home = (props) => (
         <Grid.Row>
             <LinkGroup links={LINK_GROUP_2} deviceSize={props.deviceSize}/>
         </Grid.Row>
+        <Segment basic>
+            <Image
+            src={Logo}
+            as='a'
+            size='small'
+            href='https://rightclickrva.com/business'
+            target='_blank'
+            spaced
+            />
+        </Segment>
     </Grid>
+    
 )
 export default Home
