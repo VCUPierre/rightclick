9ae24e6c6e3b599ca04cbe31cc4c9c549e208755
vcuPierre <randalltp@mymail.vcu.edu>
2020-07-16 18:29:15 -0400

    playing with background images

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React from 'react';
 import Home from './Home/components/Home'
 import { useMediaPredicate } from "react-media-hook";
 
-// import './App.css';
+import './App.css';
 
 function App() {
   const xsScreen = useMediaPredicate("(max-width: 375px)");
   const mdScreen = useMediaPredicate("(max-width: 768px)");
   const lgScreen = useMediaPredicate("(max-width: 1024px)");
 
-  return <Home deviceSize={xsScreen ? "xs" : mdScreen ? "md" : lgScreen ? "lg" : "xl"}/>
+  return <div className='backgroundImg'>
+      <Home deviceSize={xsScreen ? "xs" : mdScreen ? "md" : lgScreen ? "lg" : "xl"}/>
+    </div>
 }
 
 export default App;
