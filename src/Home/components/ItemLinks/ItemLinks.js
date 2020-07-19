3b50d6a7a60c81cc82edaea9cd166e4b07a2bf3f
vcuPierre <randalltp@mymail.vcu.edu>
2020-07-19 19:44:45 -0400

    added item link template for future functionality

---
+++ src/Home/components/ItemLinks/ItemLinks.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import { Button } from 'semantic-ui-react'
+import Banner from '../Banner/Banner'
+import '../Links/Links.css'
+
+const ItemLinks = (props) => {
+    const coloredButton = props.link.buttonColor;
+
+    return <div>
+        {props.link.featured 
+        ?
+        <div>
+            <Banner 
+                text={props.link.featuredText} 
+                color={props.link.featuredColor} 
+                bannerSide={props.link.bannerSide} 
+                deviceSize={props.deviceSize === 'xs' ? "bannerPositionS" : props.deviceSize === "md" ? "bannerPositionM" : props.deviceSize === "lg" ? "bannerPositionL" : "bannerPositionXL"}
+                timer={props.link.featuredTimerDate}
+                completeText={props.link.featuredTimerText}
+            />
+            <Button 
+                as='a' 
+                href={props.link.link}
+                {...coloredButton ? {className: 'RCPadding'}: {className: 'RCPadding RCWhiteBG'}}
+                size='big'
+                color={props.link.buttonColor}   
+                fluid>
+                    {props.link.name}
+            </Button>
+    </div>
+    :
+    <Button 
+        as='a' 
+        href={props.link.link}
+        {...coloredButton ? {className: 'RCPadding'}: {className: 'RCPadding RCWhiteBG'}}
+        size='big'
+        color={props.link.buttonColor} 
+        fluid>
+            {props.link.name}
+    </Button>
+    }
+    </div>
+}
+
+export default ItemLinks
