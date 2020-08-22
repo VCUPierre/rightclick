39b93bb66d2440c359660e7710a786c59b6e16c4
vcuPierre <randalltp@mymail.vcu.edu>
2020-08-22 15:38:06 -0400

    did some cleaning up

--- src/assets/data.js
+++ src/assets/data.js
@@ -1,57 +1,34 @@
+// Custom Colors = [RLGold,RLWhite]
 export const TITLE = {
-    Title: "Pierre Randall",
+    title: "",
     color: ""
 }
-export const PROFILE_PIC = "https://res.cloudinary.com/rightlink/image/upload/v1595178893/PierreR/Profile_gjsvbm.jpg"
+export const LOGO = "colored"
+export const PROFILE_PIC = ""
 export const BACKGROUND = {
     image: false,
     imageLink: '',
-    color: 'white',
+    color: '',
 }
 export const SOCIAL_LINKS = {
     group: [{
         name: "Instagram",
         iconName: "instagram",
-        link: "https://www.instagram.com/__pierre9",
-        color: "purple",
-        surrounded: "",
-        surroundedInColor: false,
-    },
-    {
-        name: "Twitter",
-        iconName: "twitter",
-        link: "https://twitter.com/__pierre9",
-        color: "blue",
+        link: "https://instagram.com",
+        color: "",
         surrounded: "circular",
-        surroundedInColor: false,
-    },
-    {
-        name: "LinkedIn",
-        iconName: "linkedin",
-        link: "https://www.linkedin.com/in/pierre-randall-9a1517134",
-        color: "blue",
-        surrounded: "",
-        surroundedInColor: false,
-    },
-    {
-        name: "Github",
-        iconName: "github",
-        link: "https://github.com/VCUPierre",
-        color: "black",
-        surrounded: "circular",
-        surroundedInColor: false,
+        surroundedInColor: true,
     },
 ]}
-
 // featuredTimerDate formats inclued '2020-02-01T01:02:03' or milliseconds e.g 1580518923000 - documentation here https://github.com/ndresx/react-countdown
 export const LINK_GROUP_1 = {
-    groupName: "Pesonal Links",
-    groupColor: "orange",
+    groupName: "",
+    groupColor: "",
     group: [{
-            name: "My Business",
-            link: "https://www.rightclickrva.com/business",
+            name: "",
+            link: "",
             linkSpaced: true,
-            type: "external",
+            type: "",
             typeAction: "",
             internal: false,
             featured: false,
@@ -59,275 +36,15 @@ export const LINK_GROUP_1 = {
             featuredTimerDate: "",
             featuredTimerText: "",
             featuredColor: "",
-            pinned: true,
-            mainMedia: {},
-            additionalLinks: [],  
-            color: "green",
-            colored: false,
-            buttonColor: "",
-            bannerSide: "left",
-        },
-        {
-            name: "My Portfolio",
-            link: "https://www.rightclickrva.com",
-            linkSpaced: true,
-            type: "external",
-            typeAction: "",
-            internal: false,
-            featured: false,
-            featuredText: "",
-            featuredTimerDate: "",
-            featuredTimerText: "",
-            featuredColor: "", 
             pinned: false,
             mainMedia: {},
-            additionalLinks: [],
-            color: "blue",
+            additionalLinks: [],  
+            color: "",
             colored: false,
             buttonColor: "",
-            bannerSide: "left",
-        },
-        {
-            name: "View My Resume",
-            link: "https://drive.google.com/file/d/1dMXecxJVIeuqIdfaalDJtlW8hbd_y9I-/view?usp=sharing",
-            linkSpaced: true,
-            type: "external",
-            typeAction: "download",
-            internal: false,
-            featured: false,
-            featuredText: "",
-            featuredTimerDate: "",
-            featuredTimerText: "",
-            featuredColor: "",
-            pinned: false, 
-            mainMedia: {},
-            additionalLinks: [],
-            color: "red",
-            colored: false,
-            buttonColor: "",
-            bannerSide: "left",
+            bannerSide: "",
+            additionalStyle: "",
         },
     ]
 }
 
-export const LINK_GROUP_2 = {
-    groupName: "Demo Links",
-    groupColor: "red",
-    group: [
-        {
-            name: "Internal Media",
-            link: "",
-            linkSpaced: true,
-            type: "internal",
-            typeAction: "",
-            internal: true,
-            featured: false,
-            featuredText: "", 
-            featuredTimerDate: "",
-            featuredTimerText: "",
-            featuredColor: "",
-            pinned: true,
-            mainMedia: {
-                type: "media",
-                link: "https://www.youtube.com/watch?v=UfQHEpf2q8k",
-                mediaText: "Great Cover ^^",
-            },
-            additionalLinks: [
-                {
-                    name: 'Tidal',
-                    link: 'https://tidal.com'
-                },
-                {
-                    name: 'Spotify',
-                    link: 'https://open.spotify.com'
-                },                {
-                    name: 'Apple Music',
-                    link: 'https://music.apple.com'
-                },
-            ],
-            color: "black",
-            colored: false,
-            buttonColor: "",
-            bannerSide: "left",
-        },
-        {
-            name: "Basic Banner",
-            link: "",
-            linkSpaced: true,
-            type: "internal",
-            typeAction: "",
-            internal: true,
-            featured: true,
-            featuredText: "New Banner",
-            featuredTimerDate: "",
-            featuredTimerText: "",
-            featuredColor: "red",
-            pinned: false,
-            mainMedia: {
-                type: "still",
-                link: "https://res.cloudinary.com/rightlink/image/upload/v1595189055/PierreR/image_ard339.png",
-                mediaText: "Additional Links Below",
-            },
-            additionalLinks: [
-                {
-                    name: 'Tidal',
-                    link: 'https://tidal.com'
-                },                  {
-                    name: 'Spotify',
-                    link: 'https://spotify.com'
-                },                  {
-                    name: 'Apple Music',
-                    link: 'https://music.apple.com'
-                },                  {
-                    name: 'Sound Cloud',
-                    link: 'https://soundcloud.com'
-                },                  {
-                    name: 'Youtube',
-                    link: 'https://youtube.com'
-                },             
-            ],
-            color: "red",
-            colored: false,
-            buttonColor: "",
-            bannerSide: "left",
-        },
-        {
-            name: "Countdown Banner",
-            link: "https://unitedmasters.com/m/5e99d519a2ca6d196513ca76",
-            linkSpaced: true,
-            type: "item",
-            typeAction: "",
-            internal: true,
-            featured: true,
-            featuredText: "New Content in ",
-            featuredTimerDate: "2020-12-02T00:00:00",
-            featuredTimerText: "Live now",
-            featuredColor: "green",
-            pinned: false,
-            mainMedia: {
-                type: "still",
-                link: "https://res.cloudinary.com/rightlink/image/upload/v1595189055/PierreR/image_ard339.png",
-                mediaText: "Stream Now",
-            },
-            additionalLinks: [],
-            color: "green",
-            colored: false,
-            buttonColor: "",
-            bannerSide: "left",
-        },
-        {
-            name: "Attached 1 of 2",
-            link: "",
-            linkSpaced: false,
-            type: "item",
-            typeAction: "",
-            internal: true,
-            featured: false,
-            featuredText: "", 
-            featuredTimerDate: "",
-            featuredTimerText: "", 
-            featuredColor: "",
-            pinned: false,
-            mainMedia: {},
-            additionalLinks: [
-                {
-                    name: 'Tidal',
-                    link: 'https://tidal.com'
-                },
-                {
-                    name: 'Spotify',
-                    link: 'https://spotify.com'
-                },                {
-                    name: 'Apple Music',
-                    link: 'https://applemusic.com'
-                },                {
-                    name: 'Sound Cloud',
-                    link: 'https://soundcloud.com'
-                },                {
-                    name: 'Youtube',
-                    link: 'https://youtube.com'
-                },
-            ],
-            color: "blue",
-            colored: false,
-            buttonColor: "",
-            bannerSide: "left",
-        },
-        {
-            name: "Attached 2 of 2",
-            link: "",
-            linkSpaced: true,
-            type: "item",
-            typeAction: "",
-            internal: true,
-            featured: false,
-            featuredText: "", 
-            featuredTimerDate: "",
-            featuredTimerText: "", 
-            featuredColor: "",
-            pinned: false,
-            mainMedia: {},
-            additionalLinks: [
-                {
-                    name: 'Tidal',
-                    link: 'https://tidal.com'
-                },
-                {
-                    name: 'Spotify',
-                    link: 'https://spotify.com'
-                },                {
-                    name: 'Apple Music',
-                    link: 'https://applemusic.com'
-                },                {
-                    name: 'Sound Cloud',
-                    link: 'https://soundcloud.com'
-                },                {
-                    name: 'Youtube',
-                    link: 'https://youtube.com'
-                },
-            ],
-            color: "blue",
-            colored: false,
-            buttonColor: "",
-            bannerSide: "left",
-        },
-        {
-            name: "Button Color",
-            link: "",
-            linkSpaced: true,
-            type: "item",
-            typeAction: "",
-            internal: true,
-            featured: false,
-            featuredText: "", 
-            featuredTimerDate: "",
-            featuredTimerText: "", 
-            featuredColor: "",
-            pinned: false,
-            mainMedia: {},
-            additionalLinks: [
-                {
-                    name: 'Tidal',
-                    link: 'https://tidal.com'
-                },
-                {
-                    name: 'Spotify',
-                    link: 'https://spotify.com'
-                },                {
-                    name: 'Apple Music',
-                    link: 'https://applemusic.com'
-                },                {
-                    name: 'Sound Cloud',
-                    link: 'https://soundcloud.com'
-                },                {
-                    name: 'Youtube',
-                    link: 'https://youtube.com'
-                },
-            ],
-            color: "green",
-            colored: true,
-            buttonColor: "green",
-            bannerSide: "left",
-        },
-    ]
-}
