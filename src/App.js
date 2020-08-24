e6caa5d90a19ae375c92e1cee975b290c51d1948
vcuPierre <randalltp@mymail.vcu.edu>
2020-08-23 22:18:51 -0400

    added react helmet

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import Home from './Home/components/Home'
 import { useMediaPredicate } from "react-media-hook";
 import {createUseStyles} from 'react-jss'
-import { LOGO, BACKGROUND, SOCIAL_LINKS, LINK_GROUP_1, TITLE, PROFILE_PIC } from './assets/usersData/PierreR/data.js'
+import { LOGO, BACKGROUND, SOCIAL_LINKS, LINK_GROUP_1, TITLE, PROFILE_PIC } from './assets/usersData/BenRvaMiles/data'
+import { Helmet } from 'react-helmet'
 
 const useStyles = createUseStyles({
   backgroundImg: {
@@ -11,13 +12,13 @@ const useStyles = createUseStyles({
     'background-position': '50% 95%',
     'background-attachment': 'fixed',
     'background-size': 'contain',
-  }
+  },
   //  'background-color': `${BACKGROUND.color}`
 })
 
 const linkGroups = [
   LINK_GROUP_1,
-]
+];
 
 const App = () => {
   const xsScreen = useMediaPredicate("(max-width: 375px)");
@@ -26,6 +27,16 @@ const App = () => {
   const classes = useStyles()
 
   return <div className={classes.backgroundImg}>
+      <Helmet>
+        <title>Right-Link</title>
+        <style>
+            {`
+              .RLgradient {
+                ${BACKGROUND.color} 
+               }
+            `}
+        </style>
+      </Helmet>
       <Home 
         deviceSize={xsScreen ? "xs" : mdScreen ? "md" : lgScreen ? "lg" : "xl"}
         socialLinks={SOCIAL_LINKS}
