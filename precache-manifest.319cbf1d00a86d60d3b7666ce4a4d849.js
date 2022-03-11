cc56cf7de3a4259b2709a90a90d74040042c518e
vcuPierre <randalltp@mymail.vcu.edu>
2022-03-11 18:24:09 -0500

    Updates

---
+++ precache-manifest.319cbf1d00a86d60d3b7666ce4a4d849.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d9639379e5cce1e3e6012e08a13d140",
+    "url": "/rightclick/index.html"
+  },
+  {
+    "revision": "49623d8fdc6afd0e5a65",
+    "url": "/rightclick/static/css/2.025b6a31.chunk.css"
+  },
+  {
+    "revision": "3f332cba51fca2197195",
+    "url": "/rightclick/static/css/main.a4f8802c.chunk.css"
+  },
+  {
+    "revision": "49623d8fdc6afd0e5a65",
+    "url": "/rightclick/static/js/2.1636a02a.chunk.js"
+  },
+  {
+    "revision": "347d6a74ee973323c1672186eaadb2ed",
+    "url": "/rightclick/static/js/2.1636a02a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f332cba51fca2197195",
+    "url": "/rightclick/static/js/main.1999ca10.chunk.js"
+  },
+  {
+    "revision": "6c028bc99b82a52bf9cd",
+    "url": "/rightclick/static/js/runtime-main.46b30864.js"
+  },
+  {
+    "revision": "d9d4f13402d5cbb7d7397545dc02a816",
+    "url": "/rightclick/static/media/Logo.d9d4f134.png"
+  },
+  {
+    "revision": "172852440894b31a5418d4d35063d77a",
+    "url": "/rightclick/static/media/LogoWhite.17285244.png"
+  }
+]);
